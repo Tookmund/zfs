be4652ad72c142cc483acf42a6e0bc3e8ff768f8
Jacob Adams <jacob@tookmund.com>
2021-02-03 11:56:24 -0500

    spl_migrate: migrate current process to another NUMA node

---
+++ include/spl/sys/migrate.h
@@ -0,0 +1,28 @@
+/*
+ *  Copyright (C) 2021 Jacob Adams
+ *
+ *  This file is part of the SPL, Solaris Porting Layer.
+ *  For details, see <http://zfsonlinux.org/>.
+ *
+ *  The SPL is free software; you can redistribute it and/or modify it
+ *  under the terms of the GNU General Public License as published by the
+ *  Free Software Foundation; either version 2 of the License, or (at your
+ *  option) any later version.
+ *
+ *  The SPL is distributed in the hope that it will be useful, but WITHOUT
+ *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ *  for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with the SPL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _SPL_MIGRATE_H
+#define	_SPL_MIGRATE_H
+
+#include <linux/sched.h>
+
+extern void spl_migrate(int);
+
+#endif  /* _SPL_MIGRATE_H */
