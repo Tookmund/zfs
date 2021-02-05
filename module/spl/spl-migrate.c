dacbc95e766e1cc023acfa9b4537bb4217b68f1d
Jacob Adams <jacob@tookmund.com>
2021-02-04 20:54:33 -0500

    spl-migrate: Migrate process memory across nodes

--- module/spl/spl-migrate.c
+++ module/spl/spl-migrate.c
@@ -24,6 +24,49 @@
 #include <sys/thread.h>
 #include <sys/migrate.h>
 
+/*
+ * Ripped straight from kernel_migrates_pages in linux/mm/mempolicy.c
+ * Requires do_migrate_pages to be exported
+ * Skips a bunch of security checks, because those functions
+ * aren't exported
+ */
+static inline int
+spl_migrate_pages(struct task_struct *task, int node)
+{
+	struct mm_struct *mm = NULL;
+	int err;
+	nodemask_t old = nodemask_of_node(curnode);
+	nodemask_t new = nodemask_of_node(node);
+
+	/* Find the mm_struct */
+	rcu_read_lock();
+	if (!task) {
+		rcu_read_unlock();
+		err = -ESRCH;
+		goto out;
+	}
+	get_task_struct(task);
+
+	err = -EINVAL;
+
+	rcu_read_unlock();
+
+	mm = get_task_mm(task);
+	put_task_struct(task);
+
+	if (!mm) {
+		err = -EINVAL;
+		goto out;
+	}
+
+	err = do_migrate_pages(mm, &old, &new, MPOL_MF_MOVE);
+
+	mmput(mm);
+
+out:
+	return err;
+}
+
 void
 spl_migrate(int node)
 {
@@ -34,8 +77,7 @@ spl_migrate(int node)
 	printk("SPL: Attempting  to migrate task %s from %d to %d\n",
 			curthread->comm, curnode, node);
 	set_cpus_allowed_ptr(curthread, cpumask_of_node(node));
-	kernel_migrate_pages(0, nr_node_ids, nodemask_of_node(curnode),
-			nodemask_of_node(node));
+	spl_migrate_pages(curthread, node);
 	if (curnode != node) {
 		printk("SPL: Failed to migrate task %s!\n", curthread->comm);
 		dump_stack();
